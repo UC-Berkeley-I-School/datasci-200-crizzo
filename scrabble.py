2341b7c77c4a8184dd48c30be3ac9c4a57a95a5b
crizzo <cecilia.rizzo16@gmail.com>
2023-06-16 20:37:21 -0700

    prepping files for first submission

--- scrabble.py
+++ scrabble.py
@@ -2,23 +2,30 @@ import sys
 import wordscore
 
 def run_scrabble(word):
+    if len(word) < 2 or len(word) > 7:
+        print('Too few or too many letters.')
+    if word.count('?') > 1 or word.count('*') > 1:
+        print('Too many wild cards.')
+
     words_list = load_words()
 
     potential_words = []
 
     num_total = len(word)
-    num_wild = word.count('?')
+    num_wild = word.count('?') + word.count('*')
     for single in words_list:
-        if wordscore.compare_words(word, single):
-            potential_words.append(single)
+        test = wordscore.compare_words(word, single)
+        if type(test) is tuple:
+            potential_words.append(test)
 
-    print(f'The word is *** {word} ***.')
-    print(f'The word is {num_total} letters long with {num_wild} wildcards.')
-    print(potential_words)
+    potential_words.sort(key = lambda x: x[0], reverse=True)
+    answer = [potential_words,len(potential_words)]
+
+    # print(f'The word is *** {word} ***.')
+    # print(f'The word is {num_total} letters long with {num_wild} wildcards.')
+    print(answer)
 
 def load_words():
     f = open('sowpods.txt', 'r')
     words_list = f.read().split()
     return words_list
-
-run_scrabble(sys.argv[1])
--- wordscore.py
+++ wordscore.py
@@ -1,11 +1,12 @@
 import sys
 
 def score_word(word):
-    """Scores the word based on """
+    """Scores the word based on scrabble scoring. Wildcards don't score anything"""
 
+    # Initialize scores
     scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, "x": 8, "z": 10}
 
-
+    # Check to see if the word is the right length
     if len(word) >=2 and len(word) <= 7:
         total = 0
         for char in word:
@@ -14,28 +15,33 @@ def score_word(word):
             else:
                 total += scores.get(char.lower())
         return total
-    else:
-        print('Word is either too short or too long.')
-        return 0
     
 def compare_words(letters, compare_word):
+    """See if a word can be constructed out of the letters, and if so, score it"""
+
     if len(letters) < len(compare_word):
-        print('You can\'t make a word with less letters than in the word.')
+        return False
 
     num_wilds = letters.count('?') + letters.count('*')
-    used_letters = 0
+    used_letters = ''
+    letters = letters.lower()
+    compare_word = compare_word.lower()
     
     for char in compare_word:
         if len(letters) < 1:
             break
 
         if char in letters:
-            used_letters += 1
             letters = letters.replace(char,'',1)
+            used_letters += char
 
-    print('*'*20)
-    print('used letters', used_letters)
-    
-    
+    compiled_word = used_letters + ("*" * num_wilds)
 
-compare_words(sys.argv[1], sys.argv[2])
+    if len(compiled_word) >= len(compare_word):
+        score = score_word(compiled_word)
+        together = (score, compare_word.upper())
+        #print(together)
+        return (score, compare_word.upper())
+    else:
+        return False
+
