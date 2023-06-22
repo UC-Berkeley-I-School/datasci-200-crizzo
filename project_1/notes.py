95818f63203e2bbe43209a04e222689bad1d0e00
crizzo <cecilia.rizzo16@gmail.com>
2023-06-21 20:49:37 -0700

    Adding instructor notes

---
+++ project_1/notes.py
@@ -0,0 +1,8 @@
+# Read teh document carefully
+# Actual interaction between classes isn't really intuitive
+# Don't take creative liberties with functions
+# Go through the architecture and pseudo code your way around it
+# Copy and pase sections in the Architecture into the docstring
+# You can type set the method parameters
+# but don't change the names or give default values
+# don't look up best bid algorithm online
