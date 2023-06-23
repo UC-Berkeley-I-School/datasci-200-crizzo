cc0398b160f178095b98743142a484205fcb4dd3
crizzo <cecilia.rizzo16@gmail.com>
2023-06-22 18:21:24 -0700

    HW 7-02

---
+++ test.py
@@ -0,0 +1,14 @@
+1 2 3 5 4
+2 3 5 4 1 - rotate
+3 5 4 1 2 - rotate
+5 4 1 2 3 - rotate
+4 5 1 2 3 - switch
+5 1 2 3 4 - rotate
+1 5 2 3 4 - switch
+5 2 3 4 1 - rotate
+2 5 3 4 1 - switch
+5 3 4 1 2 - rotate
+3 5 4 1 2 - switch
+5 4 1 2 3 - rotate
+
+
